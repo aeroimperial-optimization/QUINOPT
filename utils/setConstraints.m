0734ba29f65e614f0203ce028ec14319c9f0f293
Giovanni Fantuzzi <giofantuzzi@hotmail.it>
2016-09-26 15:19:17 +0100

    Added DATA.InfeasibleConstraints to list infeasibile constraints

--- utils/setConstraints.m
+++ utils/setConstraints.m
@@ -1,10 +1,34 @@
-function [CNSTR,SOS,LMIs,LINs,EQ,AuxVars,SoSParams] = setConstraints(x,Q,S,LMIs,slacks,AuxVars,EQ,opts)
+function [CNSTR,DATA,FLAG] = setConstraints(x,Q,S,LMIs,slacks,AuxVars,EQ,FLAG,opts)
 
 %% SETCONSTRAINTS.m Set constraints for QuadIntIneq
 %
 % Set the constraints for the SDP relaxation of a quadratic integral
-% inequality. Have SOS variables, linear matrix inequalities, linear
-% inequalities, equalities and a list of auxiliary variables.
+% inequality. Outputs:
+%
+% CNSTR: A structure containing the YALMIP constraints to pass to optimize. An
+%        empty structure is returned if one of the constraint is detected to be
+%        infeasible. A list of infeasible constraints is given in
+%        DATA.InfeasibleConstraints (see below)
+%
+%  DATA: A structure containing the raw data used to set up the CNSTR structure.
+%        The fields are:
+%       - SumOfSquares          : a list of polynomials to be sum-or-squares
+%       - SumOfSquaresParameters: the optimization variables in the polynomials
+%                                 listed in DATA.SumOfSquares
+%       - MatrixInequalities    : a list of matrices to be positive semidefinite
+%       - LinearInequalities    : a list of expressions to be nonnegative
+%       - Equalities            : a list of expressions to be equal to zero
+%       - slacks                : a list of the slack variables used to handle
+%                                 absolute values
+%       - auxiliaryVariables    : a list of auxiliary variables introduced by
+%                                 QUINOPT in addition to the original problem
+%                                 variables
+%       - InfeasibleConstraints : a list of variables that should be positive
+%                                 semidefinite but are not, proving that the 
+%                                 problem is trivially infeasible.
+%
+%  FLAG: An output flag: 1 if all good, 2 if at least one infeasible constraint 
+%        was detected.
 
 % ----------------------------------------------------------------------- %
 %        Author:    Giovanni Fantuzzi
@@ -14,8 +38,18 @@ function [CNSTR,SOS,LMIs,LINs,EQ,AuxVars,SoSParams] = setConstraints(x,Q,S,LMIs,
 % Last Modified:    05/05/2016
 % ----------------------------------------------------------------------- %
 
-CNSTR = [];
 
+% ----------------------------------------------------------------------- %
+% Useful stuff
+% ----------------------------------------------------------------------- %
+CNSTR = [];                 % the constraint structure
+INFCNSTR = {};              % list of infeasible constraints
+warnstr = sprintf(['\nWARNING: The current relaxation is infeasible. Try increasing the\n',...
+             'number of Legendre coefficients used in the expansion using the  \n',...
+             'input N. If the problem persists, your problem might be infeasible\n',...
+             'although a certificate of infeasibility is not available.\n\n']);
+         
+         
 % ----------------------------------------------------------------------- %
 % Infinite-dimensional part: SOS in [-1,1] or add to LMIs
 % ----------------------------------------------------------------------- %
@@ -41,54 +75,90 @@ if degree(S,x)>0
     SOS = {S-N*(1-x^2);N};
     
 elseif isa(S,'sdpvar')
-    % Add to LMIS
+    % Add to LMIs
+    LMIs{end+1,1} = S;
+    
+elseif isnumeric(S)
+    % Check for feasibility, display warning if not feasible and set 'solve'
+    % option to 'false'
+    if any(eig(S)<opts.psdtol)
+        INFCNSTR{end+1,1} = S;
+        fprintf(warnstr);
+        FLAG = 2;
+    end
+    
+    % Add to LMIs
     LMIs{end+1,1} = S;
     
 end
 
 
 % ----------------------------------------------------------------------- %
-% Linear matrix inequalities
+% Finite dimensional part (the large LMI)
 % ----------------------------------------------------------------------- %
 if isa(Q,'sdpvar');
     % Add Q to LMIs if variable, then set LMIs
     LMIs{end+1,1} = Q;
-    for i=1:length(LMIs)
-        CNSTR = [CNSTR; LMIs{i}>=0];
-    end
     
 elseif isnumeric(Q)
     % Test if it is positive definite
     E = eig((Q+Q')./2);
-    if any(E<-1e-8)
-        error('Infeasible problem (numeric LMI relaxation, not positive semidefinite).')
+    if any(E<opts.psdtol)
+        INFCNSTR{end+1,1} = Q;
+        fprintf(warnstr);
+        FLAG = 2;
     end
-    
-    % Then set LMIs
-    for i=1:length(LMIs)
-        CNSTR = [CNSTR; LMIs{i}>=0];
-    end
-    
+        
     % Then add numeric Q to list of LMIs
     LMIs{end+1,1} = Q;
 end
 
 
 % ----------------------------------------------------------------------- %
+% Linear matrix inequalities
+% ----------------------------------------------------------------------- %
+% Do not set the numeric LMIs - already tested!
+% Only set constraints if no numerical LMIs are infeasible
+if FLAG==0
+    for i=1:length(LMIs)
+        if isa(LMIs{i},'sdpvar')
+            CNSTR = [CNSTR; LMIs{i}>=0];
+        end
+    end
+end
+
+% ----------------------------------------------------------------------- %
 % Linear inequalities for slack variables
 % ----------------------------------------------------------------------- %
+% Only set constraints if FLAG==0 (no problems detected, worth trying to solve)
 LINs = [];
 if ~isempty(slacks.t)
     LINs = [slacks.t - slacks.pcoef; slacks.t + slacks.pcoef];
-    CNSTR = [CNSTR; LINs>=0];
+    if FLAG==0
+        CNSTR = [CNSTR; LINs>=0];
+    end
 end
+
 
 % ----------------------------------------------------------------------- %
 % Equality constraints from integration by parts
 % ----------------------------------------------------------------------- %
-if ~isempty(EQ)
+% Only set constraints if FLAG==0 (no problems detected, worth trying to solve)
+if ~isempty(EQ) && FLAG==0
     CNSTR = [CNSTR; EQ==0];
 end
+
+% ----------------------------------------------------------------------- %
+% Set DATA structure
+% ----------------------------------------------------------------------- %
+DATA.SumOfSquares = SOS;
+DATA.SumOfSquaresParameters = SoSParams;
+DATA.MatrixInequalities = LMIs;
+DATA.LinearInequalities = LINs;
+DATA.Equalities = EQ;
+DATA.slacks = slacks;
+DATA.auxiliaryVariables = AuxVars;
+DATA.InfeasibleConstraints = INFCNSTR;
 
 % END CODE
 end
