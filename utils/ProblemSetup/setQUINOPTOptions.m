8e341965fdd7db93f375ec4f580556c1c69b0cc6
Giovanni Fantuzzi <giofantuzzi@hotmail.it>
2016-09-26 14:36:23 +0100

    Added options.psdtol and check on class of user options

--- utils/ProblemSetup/setQUINOPTOptions.m
+++ utils/ProblemSetup/setQUINOPTOptions.m
@@ -14,11 +14,12 @@ function options = setQUINOPTOptions(userOpts)
 % ----------------------------------------------------------------------- %
 
 % Default options
-options.YALMIP           = [];        % YALMIP options
-options.solve            = true;      % Solve the optimization problem?
-options.rigorous         = true;      % Rigorous relaxation (with tail term) or simple approximation
-options.BCprojectorBasis = 'rref';    % set to 'orth' for orthonormal, 'rref' for rational
-options.sosdeg           = 6;         % degree of polynomials for S procedure
+options.YALMIP           = sdpsettings; % YALMIP options
+options.solve            = true;        % Solve the optimization problem?
+options.rigorous         = true;        % Rigorous relaxation (with tail term) or simple approximation
+options.BCprojectorBasis = 'rref';      % set to 'orth' for orthonormal, 'rref' for rational
+options.sosdeg           = 6;           % degree of polynomials for S procedure
+options.psdtol           = -1e-8;       % tolerance for small negative eigenvalues
 
 % Assign user
 if isempty(userOpts)
@@ -31,10 +32,17 @@ else
                     'rigorous'; ...
                     'BCprojectorBasis'; ...
                     'sosdeg'; ...
-                    'solve'};
+                    'solve';...
+                    'psdtol'};
     for n=1:length(fnames)
         if any(strcmpi(fnames{n},allowedNames))
-            options.(fnames{n}) = userOpts.(fnames{n});
+            boo = userOpts.(fnames{n});
+            mustbe = class(options.(fnames{n}));
+            if isa(boo,mustbe)
+                options.(fnames{n}) = boo;
+            else
+                error('Wrong class for field %s in OPTIONS.',fnames{n})
+            end
         else
             error('Unknown field %s in input OPTIONS.',fnames{n})
         end
