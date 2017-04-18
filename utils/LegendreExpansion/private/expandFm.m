e3bd6c94d2f3298f08dd9385b7e52c2c11e64906
Giovanni Fantuzzi <giofantuzzi@hotmail.it>
2017-04-18 11:43:09 +0100

    Minor edit to utils/LegendreExpansion/private/expandFm.m for minimal speed imprvement

--- utils/LegendreExpansion/private/expandFm.m
+++ utils/LegendreExpansion/private/expandFm.m
@@ -80,7 +80,7 @@ for i = 1:col
             end
             
             % Add to Fhat
-            Fhat = [Fhat; pcoef'.*sqrt(2)./sqrt( 2.*(0:pdeg)+1 ), sparse(1,Ntot-pdeg)];               
+            Fhat = [Fhat; pcoef'.*( sqrt(2)./sqrt( 2.*(0:pdeg)+1 ) ), sparse(1,Ntot-pdeg)];               
         end
         
         % Set Qm
